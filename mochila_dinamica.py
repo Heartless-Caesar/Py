bb3ab3cdf51e76bc79fedb1e13c5d434b9ff4836
João Victor Cesa Andrade <82420106+Heartless-Caesar@users.noreply.github.com>
2022-10-18 19:21:06 -0300

    Update mochila_dinamica.py

--- mochila_dinamica.py
+++ mochila_dinamica.py
@@ -5,30 +5,20 @@ aux = len(valores)
 elements = []
 
 def mochilaDinamica(capacidade,pesos,valores,tam):
-    # "m" é o dicionário que irá conter os dados
     m = {} 
     
     for i in range(capacidade+1):
         m[(0,i)] = 0
 
-    # Iterar sobre as linhas
     for i in range(1,aux + 1):
-        # Iterar sobre as colunas
+        
         for w in range(capacidade + 1):
-            # Se o peso do item for menor que o peso daquela coluna
-            # a chave e o valor passam a ser ou o valor da linha anterior ou
-            # o valor resultante da soma do valor da linha anterior somado 
-            # ao peso dos valores. 
+             
             if pesos[i - 1] <= w:
                 m[(i,w)] = max(m[(i - 1,w)], valores[i - 1] + m[(i-1,w-pesos[i - 1])])
             else:
-                # A entrada no dicionário se torna o valor da linha anterior
                 m[(i,w)] = m[(i-1,w)]
-
-    # Enquanto a capacidade for maior ou igual a zero
-    # os pesos que formam determinados para o dicionario
-    # serao adicionados a uma lista auxiliar para serem 
-    # printados          
+         
     c = capacidade
     for i in range(aux, 0, -1):
         if c - pesos[i - 1] < 0:
@@ -37,7 +27,6 @@ def mochilaDinamica(capacidade,pesos,valores,tam):
             elements.append(i)
             c -= pesos[i - 1]            
     
-    # Print da lista dos elementos presentes - 1 
     print(elements)        
 
     return m[(aux,capacidade)]    
