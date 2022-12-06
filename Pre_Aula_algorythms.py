60787c94068e542d8de145e318a8394c4c3ca62d
João Victor Cesa Andrade <82420106+Heartless-Caesar@users.noreply.github.com>
2022-12-06 19:44:46 -0300

    Update Pre_Aula_algorythms.py

--- Pre_Aula_algorythms.py
+++ Pre_Aula_algorythms.py
@@ -1,34 +1,45 @@
 import random
-import math
+
+
+
 
 def main():
-    myArr = []
 
-    print("Selecione o tamanho de vetor que será ordenado:\n1 - 5! = 120 elementos\n2 - 10! = elementos\n3 - 15! = elementos")
+    print("Selecione o tamanho de vetor que será ordenado:\n1 - 5 elementos\n2 - 10 elementos\n3 - 15 elementos")
 
     user_input = input()
 
     if user_input == '1':
-        inputAux = math.factorial(5)
+        inputAux = 5
     elif user_input == '2':
-        inputAux = math.factorial(10) 
+        inputAux = 10 
     elif user_input == '3':
-        inputAux = math.factorial(15)       
+        inputAux = 15       
     else:
         print('Input invalido')
+        
 
-    random_list = [random.randint(0,inputAux) for i in range(inputAux)]
+    random_list = [random.randint(0,inputAux + 100) for i in range(inputAux)]
+    random_list_2 = [random.randint(0,inputAux + 100) for i in range(inputAux)]
+
+    print(f"Desordenada {random_list}\n")
 
     tam = len(random_list)
 
     quick_sort(random_list, 0, tam - 1)
 
+    bubble_sort(random_list_2)
 
+    print(f"Ordenada Quick Sort {random_list}\n")
+    print(f"Comparacoes Quick = {quick_comp}")
+
+    print(f"Ordenada Bubble Sort {random_list_2}\n")
+    print(f"Comparacoes Bubble {bubble_comp}")    
 
 
 def quick_sort(list,low,high):
 
-    while low < high:
+    if low < high:
         
         pi = dividir(list, low, high)
 
@@ -36,19 +47,35 @@ def quick_sort(list,low,high):
 
         quick_sort(list,pi + 1, high)
 
+        
+
 
 def dividir(arr, low, high):
     pivo = arr[high]
-
+    quick_comp = 0
     i = low - 1
 
     for j in range(low, high):
         if arr[j] <= pivo:
+            quick_comp = quick_comp + 1
             i = i + 1
 
             (arr[i], arr[j]) = (arr[j], arr[i])
 
-        (arr[i + 1], arr[high]) = (arr[high], arr[i + 1])
-
+    (arr[i + 1], arr[high]) = (arr[high], arr[i + 1])
+    
     return i + 1
 
+
+def bubble_sort(arr):
+    bubble_comp = 0
+    for i in range(len(arr) - 1):
+        for j in range(0, len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                bubble_comp = bubble_comp + 1
+                temp = arr[j + 1]
+                arr[j + 1] = arr[j]
+                arr[j] = temp
+
+
+main()
